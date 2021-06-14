9310ba6faf8022eb8b253b163a80c6565cc32a04
Anderson H <anderson-e-s@outlook.com>
2021-06-14 11:51:33 -0300

    Adicionada a funçao de decifrar mensages

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-
 def encrypt(plain_text, shift_amount):
     cipher_text = ""
     for letter in plain_text:
@@ -14,4 +13,15 @@ def encrypt(plain_text, shift_amount):
         cipher_text += new_letter
     print(f"The encoded text is {cipher_text}")
 
-encrypt(plain_text = text, shift_amount = shift)
+def decrypt(cipher_text, shift_amount):
+    plain_text = ""
+    for letter in cipher_text:
+        position = alphabet.index(letter)
+        new_position = position - shift_amount
+        plain_text += alphabet[new_position]       
+    print(f"The decoded text is {plain_text}")
+
+if direction == 'encode':
+    encrypt(plain_text = text, shift_amount = shift)
+elif direction == 'decode':
+    decrypt(cipher_text = text, shift_amount = shift)
